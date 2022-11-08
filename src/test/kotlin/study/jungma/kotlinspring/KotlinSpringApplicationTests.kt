10f102a721c61b52d2bc5e8d3f15ca1a9267f461
Jungma <mbjun3630@naver.com>
2022-11-08 13:29:07 +0900

    Initial commit

---
+++ src/test/kotlin/study/jungma/kotlinspring/KotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.jungma.kotlinspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
